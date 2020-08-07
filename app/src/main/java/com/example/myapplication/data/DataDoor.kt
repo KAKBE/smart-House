5598ab6b024ef6c940859b4667eaae60f821db1a
KAKBE <veretnov999@mail.ru>
2020-08-07 14:20:54 +0300

    Добавили тестовый фрагмент4

---
+++ app/src/main/java/com/example/myapplication/data/DataDoor.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.data
+
+class DataDoor (
+    val state : Boolean
+)
+
+class DataDoorPhoto (
+    val photo : String
+)
+
+class LightHistory(
+    val history: Array<DoorHistoryItem>
+)
---
+++ app/src/main/java/com/example/myapplication/data/DataLight.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication.data
+
+class DataLight (
+    val state : Boolean,
+    val levelMin: Int,
+    val levelMax: Int
+
+)
+
+class DataLightSleep (
+    val state : Boolean,
+    val levelMin: Int,
+    val levelMax: Int,
+    val time: String
+
+)
